a81e1ea8049ccf7c4056393de1e63ca237d10998
nodiscc <nodiscc@gmail.com>
2012-10-13 00:39:59 +0200

    some progress on deb-screenshot, add manpages to manpagestopdf

--- dev/get-debscreenshot.sh
+++ dev/get-debscreenshot.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #For each specified package, fetches a large screenshot from s.d.n and places it in a subdir
-#TODO: test everything, enhance everything
+#TODO: Declare user agent, pause between requests, create thumbnails
 
-for PACKAGE in pybackpack osmo evolution
+
+for PACKAGE in pybackpack osmo evolution icedove iceweasel owncloud filezilla
 do
-	mkdir $PACKAGE
-	cd $PACKAGE
+#	mkdir $PACKAGE
+#	cd $PACKAGE
 	for SCREENSHOT in `curl -s -f http://screenshots.debian.net/package/$PACKAGE | grep "large.png" | cut -f 4 -d "\""`
-	do wget http://screenshots.debian.net/$SCREENSHOT
+		do wget -O $PACKAGE-`basename $SCREENSHOT` http://screenshots.debian.net/$SCREENSHOT
 	done
-	cd ..
+#	cd ..
 done
 
