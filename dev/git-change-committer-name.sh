9826767b867e431d44b3d29a1eaa94dadf9c0a7f
nodiscc <nodiscc@gmail.com>
2012-10-13 00:47:34 +0200

    add git change commiter name script

---
+++ dev/git-change-committer-name.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+an="$GIT_AUTHOR_NAME"
+am="$GIT_AUTHOR_EMAIL"
+cn="$GIT_COMMITTER_NAME"
+cm="$GIT_COMMITTER_EMAIL"
+
+if [ "$GIT_COMMITTER_EMAIL" = "nodiscc@gmail.com" ]
+then
+    cn="badsuperblock"
+    cm="badsuperblock@telecom.dmz.se"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "nodiscc@gmail.com" ]
+then
+    an="badsuperblock"
+    am="badsuperblock@telecom.dmz.se"
+fi
+
+export GIT_AUTHOR_NAME="$an"
+export GIT_AUTHOR_EMAIL="$am"
+export GIT_COMMITTER_NAME="$cn"
+export GIT_COMMITTER_EMAIL="$cm"
+'
+
