0338d2cfe0818bd100d38f5f3effa386c60050ae
nodiscc <nodiscc@gmail.com>
2012-11-10 12:31:46 +0100

    random words: use english dictionnary

--- utility/3-random-words.sh
+++ utility/3-random-words.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function checkword() {
-sed `perl -e "print int rand(99999)"`"q;d" /usr/share/hunspell/fr_FR.dic >> checkword
+sed `perl -e "print int rand(99999)"`"q;d" /usr/share/hunspell/en_US.dic >> checkword
 }
 
 while [ `cat checkword 2>/dev/null | wc -l` -ne "3" ]; do checkword; done
