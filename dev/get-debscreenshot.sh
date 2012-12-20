cc882b23e7fdc84dd89cf4bb02c6a1a4374ad303
nodiscc <nodiscc@gmail.com>
2012-12-20 01:54:28 +0100

    report errors

--- dev/get-debscreenshot.sh
+++ dev/get-debscreenshot.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 #For each specified package, fetches a large screenshot from s.d.n and places it in a subdir
-#TODO: create thumbnails, report errors
+#TODO: colorize output
+#TODO: use 3 directories (rejected, ok, new)
+#TODO: report errors (CURLOPTS)
+
 
 USERAGENT="Mozilla/4.0, debscreenshotgrabber"
-#PACKAGES=
+#PACKAGES=$(cat $1|egrep -v "\#"|tr "\n" " ")
+PACKAGES="$@"
 NEWFILES=""
+NOTFOUND=""
 
-for PACKAGE in pybackpack osmo evolution icedove iceweasel owncloud filezilla synapse volumeicon-alsa xpad xfce4-power-manager gtk-redshift network-manager-gnome tint2 gnome-terminal nautilus audacious liferea pidgin iceweasel transmission-gtk file-roller mousepad gedit eog lxappearance bleachbit eog gnome-control-center pdfmod gnome-system-monitor evince icedove soundjuicer brasero gnome-screenshot gnome-activity-journal
+for PACKAGE in $PACKAGES
 do
 	for SCREENSHOT in `curl -s -A "$USERAGENT" -f http://screenshots.debian.net/package/$PACKAGE | grep "large.png" | cut -f 4 -d "\""`
-	do FILENAME=$PACKAGE-`basename $SCREENSHOT`
+	do
+		FILENAME=$PACKAGE-`basename $SCREENSHOT`
 		if [ -f $FILENAME ]
 		then echo "File $FILENAME already exists, skipping..."
-		else wget -U "$USERAGENT" -nv -O $FILENAME http://screenshots.debian.net/$SCREENSHOT; NEWFILES="$NEWFILES $FILENAME"; sleep 3;
+		else wget -U "$USERAGENT" -nv -O $FILENAME http://screenshots.debian.net/$SCREENSHOT
+		NEWFILES="$NEWFILES $FILENAME"
+		sleep 3;
 		fi
 	done
+	ls $PACKAGE* &>/dev/null
+	PACKAGEFOUND="!$"
+	if [ "$PACKAGEFOUND" != "0" ]
+	then echo -e "\033[00;31mno screenshots for $PACKAGE\033[00m"
+	NOTFOUND="$NOTFOUND $PACKAGE"
+	fi
 done
 
 echo "Newly retrieved files: $NEWFILES"
-echo "Packages without screenshots: "
+echo  -e "\033[00;31mPackages without screenshots: $NOTFOUND\033[00m"
