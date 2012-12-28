c663ff9f58186088313d1d91ab0263161fc118c7
nodiscc <nodiscc@gmail.com>
2012-12-28 14:12:40 +0100

    add description, update committer name

--- dev/git-change-committer-name.sh
+++ dev/git-change-committer-name.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-#Description: change
+#Description: change committer name/email for all commits in a git repo
 #Source: https://help.github.com/articles/changing-author-info
 
-git filter-branch --env-filter '
+git filter-branch -f --env-filter '
 
 an="$GIT_AUTHOR_NAME"
 am="$GIT_AUTHOR_EMAIL"
@@ -11,13 +11,13 @@ cm="$GIT_COMMITTER_EMAIL"
 
 if [ "$GIT_COMMITTER_EMAIL" = "$GITEMAIL" ]
 then
-    cn="badsuperblock"
-    cm="badsuperblock@telecom.dmz.se"
+    cn="nodiscc"
+    cm="nodiscc@gmail.com"
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "nodiscc@gmail.com" ]
+if [ "$GIT_AUTHOR_EMAIL" = "badsuperblock@telecom.dmz.se" ]
 then
-    an="badsuperblock"
-    am="badsuperblock@telecom.dmz.se"
+    an="nodiscc"
+    am="nodiscc@gmail.com"
 fi
 
 export GIT_AUTHOR_NAME="$an"
