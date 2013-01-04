a72449fa6aa629221a6189ee2e7ea3f51896a978
nodiscc <nodiscc@gmail.com>
2013-01-05 00:03:49 +0100

    refactoring: use bash functions, add streaming ability

--- web/htmlbookmarks-media-fetcher.sh
+++ web/htmlbookmarks-media-fetcher.sh
@@ -5,12 +5,19 @@
 #TODO: Allow not only downloading media, but also text/html files like Scrapbook does
 #TODO: report errors
 #TODO: test if we are in a pts/tty. If not, use zenity as a frontend (if [ `tty` != "*/dev/*" ......)
+#TODO: send title as detected by quvi (%t) to mplayer (-title) or vlc (--meta-title)
 
-USAGE="USAGE: `basename $0` /path/to/bookmarks/file.html /path/to/download/dir/ [tag]"
-BOOKMARKFILE="$1"
-DEST_DIR="$2"
-TAGS="$3"
+#TODO SOURCE="" #can be any of Webpage, Netscape/Firefox/Delicious bookmark format (supports tags), Simple list
+#TODO QUALITY="" #can be any of low/medium/high
+USAGE="USAGE: `basename $0` [download|stream] /path/to/bookmarks/file.html [/path/to/download/dir/] [tag]"
+ACTION="$1"
+BOOKMARKFILE="$2"
+DEST_DIR="$3"
+TAGS="$4"
 SUPPORTEDSITES=`cclive --support | tr "\n" "\|" | awk 'sub(".$", "")'`
+#PLAYCOMMAND="vlc --playlist-enqueue %u &"
+PLAYCOMMAND="mplayer %u -title %t"
+#PLAYCOMMAND="vlc --playlist-enqueue %u"
 RED="\033[00;31m"
 GREEN="\033[00;32m"
 ENDCOLOR="\033[00m"
@@ -27,16 +34,11 @@ if [ ! -f "$BOOKMARKFILE" ]
 	exit 1
 fi
 
-if [ ! -d "$DEST_DIR" ]
-	then echo -e "${RED}Error: you did not specify a destination directory, or it is not valid${ENDCOLOR}"
-	echo "$USAGE"
-	exit 1
-fi
 
-#No tags entered, interactive tag selection
+function SelectTags() {
 if [ "$TAGS" = "" ]
 	then echo -e \
-"What do you want to download?
+"What do you want to download/stream?
 ${GREEN}[1]${ENDCOLOR} music
 ${GREEN}[2]${ENDCOLOR} videos
 ${GREEN}[3]${ENDCOLOR} other (free keyword)"
@@ -58,9 +60,42 @@ case $TAGSELECTION in
 	  ;;
 esac
 fi
+}
 
-#Go download!
-#grep ".*["$SUPPORTEDSITES"].*TAGS=.*"$TAGS".*" $BOOKMARKFILE | awk -F "\"" '{print $2}' OLD CODE, WHAT DOES IT DO?
+function Parse() {
+egrep -w "$SUPPORTEDSITES" $BOOKMARKFILE | egrep "TAGS=.*$TAGS.*" | awk -F "\"" '{print $2}'
+}
+
+
+function Download() {
+if [ ! -d "$DEST_DIR" ]
+	then echo -e "${RED}Error: you did not specify a destination directory, or it is not valid${ENDCOLOR}"
+	echo "$USAGE"
+	exit 1
+fi
+
+SelectTags
 cd "$DEST_DIR"
 echo -e "Downloading ${GREEN}$TAGS${ENDCOLOR} related items from ${GREEN}$BOOKMARKFILE${ENDCOLOR}. Saving to ${GREEN}$DEST_DIR${ENDCOLOR}"
-egrep -w "$SUPPORTEDSITES" $BOOKMARKFILE | egrep "TAGS=.*$TAGS.*" | awk -F "\"" '{print $2}' | xargs cclive
+Parse | xargs cclive --continue
+}
+
+
+function Stream() {
+SelectTags
+echo -e "Streaming ${GREEN}$TAGS${ENDCOLOR} related items from ${GREEN}$BOOKMARKFILE${ENDCOLOR}."
+Parse | xargs quvi --exec="$PLAYCOMMAND"
+}
+
+case "$ACTION" in
+	"download")
+	Download
+	;;
+	"stream")
+	Stream
+	;;
+	*)
+	echo "$USAGE"
+	echo "Action $ACTION is not a valid action"
+	;;
+esac
