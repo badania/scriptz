b2ab0b0b17ea328e901515e79eec4f9ef7d80db7
nodiscc <nodiscc@gmail.com>
2013-01-19 15:53:14 +0100

    use 4 directories (./ for new screenshots, accepted/ uploaded/ and rejected/)

--- dev/get-debscreenshot.sh
+++ dev/get-debscreenshot.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #For each specified package, fetches a large screenshot from s.d.n and places it in a subdir
 #TODO: colorize output
-#TODO: use 3 directories (rejected, ok, new)
 #TODO: report errors (CURLOPTS)
 
 
 USERAGENT="Mozilla/4.0, debscreenshotgrabber"
+SCREENSHOTSDIR="$PWD"
 #PACKAGES=$(cat $1|egrep -v "\#"|tr "\n" " ")
 PACKAGES="$@"
 NEWFILES=""
@@ -16,14 +16,14 @@ do
 	for SCREENSHOT in `curl -s -A "$USERAGENT" -f http://screenshots.debian.net/package/$PACKAGE | grep "large.png" | cut -f 4 -d "\""`
 	do
 		FILENAME=$PACKAGE-`basename $SCREENSHOT`
-		if [ -f $FILENAME ]
+		if [ -f $FILENAME -o -f "$SCREENSHOTSDIR/accepted/$FILENAME" -o -f "$SCREENSHOTSDIR/rejected/$FILENAME" -o -f "$SCREENSHOTSDIR/uploaded/$FILENAME" ]
 		then echo "File $FILENAME already exists, skipping..."
 		else wget -U "$USERAGENT" -nv -O $FILENAME http://screenshots.debian.net/$SCREENSHOT
 		NEWFILES="$NEWFILES $FILENAME"
 		sleep 3;
 		fi
 	done
-	ls $PACKAGE* &>/dev/null
+	ls $SCREENSHOTSDIR/$PACKAGE* &>/dev/null #TODO: this is bad
 	PACKAGEFOUND="!$"
 	if [ "$PACKAGEFOUND" != "0" ]
 	then echo -e "\033[00;31mno screenshots for $PACKAGE\033[00m"
