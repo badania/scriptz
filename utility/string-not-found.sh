b73e4f2dcdf15608b62819780ff58762c683a66c
nodiscc <nodiscc@gmail.com>
2013-01-20 15:03:11 +0100

    ability to search in multiple files

--- utility/string-not-found.sh
+++ utility/string-not-found.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-#Description: takes items from a list ($1) and searches for these items
-#in a second file ($2). If an item is not found in the second file, output
-#an error
+#Description: takes items from a list (1st argument) and searches for these items
+#in other filese (arguments starting from 2nd). If an item is not found in the second file
+#output, an error.
 #License: MIT (http://opensource.org/licenses/MIT)
 #Copyright: Rxtx Project <nodiscc@gmail.com>
 
+USAGE="Usage: `basename $0` [item_list_file] [files_to_search in]"
 ITEM_LIST="$1"
-FILE_TO_SEARCH="$2"
+PARAMS=($@)
+FILE_TO_SEARCH="${PARAMS[@]:1}"
+
+if [ "$1" = "" -o "$1" = "-h" -o "$2" = "" ]
+then
+	echo "$USAGE"
+	exit 1
+fi
 
 for item in `cat $ITEM_LIST`
 do
