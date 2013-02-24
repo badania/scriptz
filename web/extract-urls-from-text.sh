c2425fbc9453ac41039ec49352da78df6f396e13
nodiscc <nodiscc@gmail.com>
2013-02-24 23:57:03 +0100

    extract-urls-from-text: zalgo mode

--- web/extract-urls-from-text.sh
+++ web/extract-urls-from-text.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 #TODO: everything.
 #I did not find how to do this. For now, use http://pgl.yoyo.org/urlex/
-#Best luck I had: grep -o '^http*[ ^]' $FILE
+#Best luck I had:
+FILE="$1"
+grep -o 'http.*"' "$FILE"
