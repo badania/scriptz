bfa2b5f61a300f4edb0045474cd8968b9222ccc1
nodiscc <nodiscc@gmail.com>
2013-03-04 14:06:19 +0100

    webpage-to-markdown: add USAGE

--- web/webpage-to-markdown.sh
+++ web/webpage-to-markdown.sh
@@ -2,5 +2,11 @@
 #fetches a webpage and converts it to markdown
 URL="$1"
 OUTFILE="$2"
+USAGE="USAGE: `basename $0` page_url output_file"
+
+if [ "$1" = "" -o "$2" = "" ]
+	then echo "$USAGE"
+	exit 1
+fi
 
 pandoc -f html -t markdown "$URL" > "$OUTFILE"
