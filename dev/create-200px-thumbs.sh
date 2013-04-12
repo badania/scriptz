b8f6fc58b3d87051e8e9f9606d96b094d110d67c
nodiscc <nodiscc@gmail.com>
2013-04-12 14:21:45 +0200

    mve create-200px-thumbs to code-samples repo

--- dev/create-200px-thumbs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#Description: create a 200px thumbnail for each .png image here, write it in a thumbs/ subdir.
-
-mkdir thumbs
-for image in *.png; do
-if [ ! -e thumbs/"$image"_th.png ] #do not re-create it if it exists
-	then convert -resize 200x "$image" thumbs/`basename "$image" .png`_th.png
-fi
-done
