0c9195ba2576172e85daf5a69bb9f378918a1e6b
nodiscc <nodiscc@gmail.com>
2013-05-27 23:37:01 +0200

    cloudbackup: add -m (module) and -l (list modules) options

--- web/cloudbackup/cloudbackup.sh
+++ web/cloudbackup/cloudbackup.sh
@@ -3,6 +3,9 @@
 #Dependencies: googlecl, curl, python-jsonpipe, xmlstarlet
 #and of course grep, awk, cut, sed, coreutils...
 
+#Global variables
+MODULES_PATH=`dirname $0`
+
 #Usernames
 GITHUB_USER=""
 DEVIANTART_USER=""
@@ -19,7 +22,26 @@ LASTFM_APIKEY="9b6009eca365ded3a03c2b9673d54eb9"
 REDDIT_PASSWD=""
 
 
-###TODO: print available modules with -l
+while getopts ":m:l" opt; do
+  case $opt in
+    m)
+      bash "$MODULES_PATH"/"$OPTARG".module
+      ;;
+	l) #print available modules
+	  find "$MODULES_PATH" -name "*.module" -exec basename '{}' \;
+	  ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Usage: -$OPTARG must have an argument" >&2
+      exit 1
+      ;;
+  esac
+done
+
+
 ###TODO: put the variables (usernames;passwords) in a .config file
 ###TODO: fail if a requested module has no username/password set for it
 ###TODO: allow selecting modules with `-m modulename.module -m another.module`, default to all modules
